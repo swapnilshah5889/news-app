13d910a3ed4216ae86583047ab9959dc633fea8e
Swapnil Shah <swapnilshah5889@gmail.com>
2023-08-21 16:43:16 -0400

    Implemented react router, set default route container as the news homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Home } from '@mui/icons-material'
 import './App.css'
 import HomePage from './pages/home.page/home.page'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route index path='/' element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
